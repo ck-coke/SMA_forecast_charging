7e3bede4883f28b6bedd904897bae3b8aa00b829
arteck <6681528+arteck@users.noreply.github.com>
2024-01-07 08:43:28 +0100

    Update get_tibber_data_0.1.js

--- SUNNY_TRIPOWER _10.0_SE/get_tibber_data_0.1.js
+++ SUNNY_TRIPOWER _10.0_SE/get_tibber_data_0.1.js
@@ -1,39 +1,31 @@
-const _tibber = 'tibberlink.0.Homes.cccccccccccccccccccccccccccccc.';     // Anpassen!
+const _tibber = 'tibberlink.0.Homes.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.';     // Anpassen!
 const _tibberDP1 = '0_userdata.0';
 const _tibberDP2 = 'strom.tibber.';
 const _tibberDP = _tibberDP1 + '.' + _tibberDP2;
+const heute  = 'PricesToday.';
+const morgen = 'PricesTomorrow.';
 
-
-createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.tibberBestPreisArray', { 'name': 'tibber bester preis als array', 'type':'array', 'read': true, 'write': false, 'role': 'object'}], function () {});       
-
-createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.tibberBestPreisArrayLang', { 'name': 'tibber bester preis als array', 'type':'array', 'read': true, 'write': false, 'role': 'object'}], function () {}); 
-
-createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.batterieLadenHH', { 'name': 'max ladeleistung', 'type':'number', 'read': true, 'write': false, 'role': 'state', 'def':0 }], function () {        
+createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.tibberBestPreisArray', { 'name': 'tibber bester preis als array', 'type':'array', 'read': true, 'write': false, 'role': 'object'}], function () {  });       
+createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.tibberBestPreisArrayLang', { 'name': 'tibber bester preis als array', 'type':'array', 'read': true, 'write': false, 'role': 'object'}], function () {  }); 
+createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.batterieLadenHH', { 'name': 'batterieLadenHH', 'type':'number', 'read': true, 'write': false, 'role': 'state', 'def':0 }], function () {        
         setState(_tibberDP + 'extra.batterieLadenHH', 0, true);
 }); 
-createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.tibberBestPreis', { 'name': 'max ladeleistung', 'type':'number', 'read': true, 'write': false, 'role': 'state', 'def':0 }], function () {        
+createUserStates(_tibberDP1, false, [_tibberDP2 + 'extra.tibberBestPreis', { 'name': 'tibberBestPreis', 'type':'number', 'read': true, 'write': false, 'role': 'state', 'def':0 }], function () {        
     setState(_tibberDP + 'extra.tibberBestPreis', 0, true);
 }); 
 
+holePreis(heute,heute);
 
-holePreis();
-
-function holePreis() {
+function holePreis(preisHeute,preisMorgen) {
     let preise = [];
     const options = { hour12: false, hour: '2-digit', minute:'2-digit'};
-    const jetzt = new Date();
-    const midn = new Date(
-                    jetzt.getFullYear(),
-                    jetzt.getMonth(),
-                    jetzt.getDate(),
-                    0,0,0);
-                                  
+   
     for(let i = 14; i < 24; i++) {      
         let obj = {};
         
         const stateBaseName = "strom.tibber." + i + ".";
-        const idStart = _tibber + 'PricesToday.' + i + '.startsAt';
-        const idPreis = _tibber + 'PricesToday.' + i + '.total';
+        const idStart = _tibber + preisHeute + i + '.startsAt';
+        const idPreis = _tibber + preisHeute + i + '.total';
         const startsAt  = getState(idStart).val;
         const start = new Date(startsAt);        
         const end = new Date(Date.parse(startsAt)).getTime()+3600000;            
@@ -45,6 +37,8 @@ function holePreis() {
         const preis =  getState(idPreis).val;
         obj.preis = preis;
         preise.push(obj);
+        const level = _tibber + preisHeute + i + '.level';
+        const levelText  = getState(level).val;
 
         createUserStates('0_userdata.0', false, [stateBaseName + 'startTime', { 'name': 'Gultigkeitsbeginn (Uhrzeit)', 'type':'string', 'read': true, 'write': false, 'role': 'state' }], function () {        
             setState('0_userdata.0.' + stateBaseName + 'startTime', startTime, true);
@@ -57,14 +51,17 @@ function holePreis() {
         });
         createUserStates('0_userdata.0', false, [stateBaseName+ 'price', { 'name': 'Preis', 'type':'number', 'read': true, 'write': false, 'role': 'state',  'def':0 }], function () {        
             setState('0_userdata.0.' + stateBaseName + 'price', preis, true);
-        });                  
+        });   
+        createUserStates('0_userdata.0', false, [stateBaseName+ 'level', { 'name': 'Preis Level', 'type':'string', 'read': true, 'write': false, 'role': 'text',  'def': '' }], function () {        
+            setState('0_userdata.0.' + stateBaseName + 'level', levelText, true);
+        });  
     }
     
     for(let i = 0; i < 14; i++) {
         let obj = {};
         const stateBaseName = "strom.tibber." + i + ".";
-        const idStart = _tibber + 'PricesTomorrow.' + i + '.startsAt';
-        const idPreis = _tibber + 'PricesTomorrow.' + i + '.total';
+        const idStart = _tibber + preisMorgen + i + '.startsAt';
+        const idPreis = _tibber + preisMorgen + i + '.total';
         const startsAt  = getState(idStart).val;
         const start = new Date(startsAt);        
         const end = new Date(Date.parse(startsAt)).getTime()+3600000;            
@@ -76,6 +73,9 @@ function holePreis() {
         const preis =  getState(idPreis).val;
         obj.preis = preis;
         preise.push(obj);
+    
+        const level = _tibber + preisMorgen + i + '.level';
+        const levelText  = getState(level).val;
 
         createUserStates('0_userdata.0', false, [stateBaseName + 'startTime', { 'name': 'Gultigkeitsbeginn (Uhrzeit)', 'type':'string', 'read': true, 'write': false, 'role': 'state' }], function () {        
             setState('0_userdata.0.' + stateBaseName + 'startTime', startTime, true);
@@ -89,12 +89,15 @@ function holePreis() {
         createUserStates('0_userdata.0', false, [stateBaseName+ 'price', { 'name': 'Preis', 'type':'number', 'read': true, 'write': false, 'role': 'state',  'def':0 }], function () {        
             setState('0_userdata.0.' + stateBaseName + 'price', preis, true);
         });  
+        createUserStates('0_userdata.0', false, [stateBaseName+ 'level', { 'name': 'Preis Level', 'type':'string', 'read': true, 'write': false, 'role': 'text',  'def': '' }], function () {        
+            setState('0_userdata.0.' + stateBaseName + 'level', levelText, true);
+        });  
     }
 
     errechneBesteUhrzeit(preise);
 
     preise.sort(function(a, b) {
-        return a.preis - b.preis;
+        return a.start - b.start;
     });
 
     setState(_tibberDP + 'extra.tibberBestPreisArrayLang', preise, true);
@@ -131,6 +134,27 @@ function startZeit(preiseKurz) {
     setState(_tibberDP + 'extra.tibberBestPreisArray', preiseKurz, true);
 }
 
-schedule('57 13 * * *', function() {
-    holePreis();      
+
+schedule('*/60 14-23 * * *', function() {
+    holePreis(heute,morgen);     
+});
+
+schedule('*/60 0-14 * * *', function() {
+    holePreis(heute,heute);     
+});
+
+schedule('0 * * * *', function() {
+    let stunde = getHH();       
+    setState('0_userdata.0.strom.tibber.extra.tibberJetzt' , getState('0_userdata.0.strom.tibber.' + stunde + '.price'/*Preis*/).val, true);
+    setState('0_userdata.0.strom.tibber.extra.tibberJetztLevel' , getState('0_userdata.0.strom.tibber.' + stunde + '.level'/*level*/).val, true);
+    
+    stunde += 1;
+    if (stunde == 24) {
+        stunde = 0;
+    }
+
+    setState('0_userdata.0.strom.tibber.extra.tibberNächsteStunde' , getState('0_userdata.0.strom.tibber.' + stunde + '.price'/*Preis*/).val, true);
+    setState('0_userdata.0.strom.tibber.extra.tibberNächsteStundeLevel' , getState('0_userdata.0.strom.tibber.' + stunde + '.level'/*level*/).val, true);
+
+
 });
