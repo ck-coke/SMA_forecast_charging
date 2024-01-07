55e045c96dbe4967d43ba9fc2ccb2cdb70a36e49
arteck <6681528+arteck@users.noreply.github.com>
2024-01-07 08:40:27 +0100

    Update get_solcast_data_0.1.js

--- SUNNY_TRIPOWER _10.0_SE/get_solcast_data_0.1.js
+++ SUNNY_TRIPOWER _10.0_SE/get_solcast_data_0.1.js
@@ -1,98 +1,81 @@
 
-const axios   = require('axios');
-const https   = require('https');
-const method  = 'GET';
-const baseUri = 'https://api.solcast.com.au';
+const axios = require('axios'); // Import the Axios library
 
-const garten  = "zzzzzzzzzzzzzzzzzzzz";  // 
-const strasse = "yyyyyyyyyyyyyyyyyyyyy";  // 
+const garten = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const strasse = "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy";
 
-const key_id = "xxxxxxxxxxxxxxxxxxxxxx";
+const key_id = "zzzzzzzzzzzzzzzzzzzzzzzzzzz";
 let hours = 24;
 
-const qurl = "/rooftop_sites/" + garten +"/forecasts?format=json&api_key="+ key_id;
-    
-    requestData(qurl);
+const baseUrl = "https://api.solcast.com.au/rooftop_sites/";
 
-function requestData(urlReq) {
-    console.log('URI ' + urlReq);
+function requestData(seiteUrl) {
+    const url = `${baseUrl}${seiteUrl}`
 
-    axios({
-        method,
-        baseURL: baseUri,
-        url: urlReq,     
-        timeout: 5000, // wait for 5s       
-        validateStatus: (status) => {
-            return [200].indexOf(status) > -1;
-        },
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false,
-        }),
-        
-    })
-    .then((response) => {  
-        const array = JSON.parse(response.data.result).forecasts;
-      
-        var list = [];
-        for(let i = 0; i < array.length; i++) {
-            var endtime = Date.parse(array[i].period_end);
-            var time = new Date(endtime-1800000);
-            var readpwr = array[i].pv_estimate;
-            var readpwr90 = array[i].pv_estimate90;
-            list[i] = {};
-            list[i].time = time/1000;
-            list[i].watt = Math.round(readpwr * 1000);
-            list[i].watt90 = Math.round(readpwr90 * 1000);
-   //        console.log(list[i].time + ' ' + list[i].watt + ' ' + list[i].watt90);
-        };
-                
-        for(let a = 0; a < (hours*2); a++) {
-            let start = new Date((list[a].time)*1000);
-            let end = new Date(((list[a].time)*1000)+1800000)
-            var options = { hour12: false, hour: '2-digit', minute:'2-digit'};
-            let startTime = start.toLocaleTimeString('de-DE', options);
-            let endTime = end.toLocaleTimeString('de-DE', options);
 
-            //   console.log(startTime + ',' + endTime + ',' + list[a].watt + ',' + list[a].watt90);
+    axios
+        .get(url)
+        .then((response) => {
+            if (response.status === 200) {
+                const array = response.data.forecasts;
+                const list = [];
 
-            let stateBaseName = "strom.pvforecast." + a + ".";
+                for (let i = 0; i < array.length; i++) {
+                const endtime = Date.parse(array[i].period_end);
+                const time = new Date(endtime - 1800000);
+                const readpwr = array[i].pv_estimate;
+                const readpwr90 = array[i].pv_estimate90;
 
-            createUserStates('0_userdata.0', false, [stateBaseName + 'startTime', { 'name': 'Gultigkeitsbeginn (Uhrzeit)', 'type':'string', 'read': true, 'write': false, 'role': 'state' }], function () {        
-                setState('0_userdata.0.' + stateBaseName + 'startTime', startTime, true);
-            });
-            createUserStates('0_userdata.0', false, [stateBaseName+ 'endTime', { 'name': 'Gultigkeitsende (Uhrzeit)', 'type':'string', 'read': true, 'write': false, 'role': 'state' }], function () {        
-                setState('0_userdata.0.' + stateBaseName + 'endTime', endTime, true);
-            });
+                list[i] = {
+                    time: time / 1000,
+                    watt: Math.round(readpwr * 1000),
+                    watt90: Math.round(readpwr90 * 1000),
+                };
+                }
 
-            createUserStates('0_userdata.0', false, [stateBaseName+ 'power', { 'name': 'power', 'type':'number', 'read': true, 'write': false, 'role': 'state',  'def':0 }], function () {        
-                setState('0_userdata.0.' + stateBaseName + 'power', list[a].watt, true);
-            });  
+                for (let a = 0; a < hours * 2; a++) {
+                const start = new Date(list[a].time * 1000);
+                const end = new Date(list[a].time * 1000 + 1800000);
 
-            createUserStates('0_userdata.0', false, [stateBaseName+ 'power90', { 'name': 'power90', 'type':'number', 'read': true, 'write': false, 'role': 'state',  'def':0 }], function () {        
-                setState('0_userdata.0.' + stateBaseName + 'power90', list[a].watt90, true);
-            });                
-        }
-    })
-    .catch((error) => {   
-        console.log('ERROR pv forecast ' + error);   
+                const options = { hour12: false, hour: '2-digit', minute: '2-digit' };
+                const startTime = start.toLocaleTimeString('de-DE', options);
+                const endTime = end.toLocaleTimeString('de-DE', options);
+
+                const stateBaseName = "strom.pvforecast." + a + ".";
+
+                createUserStates('0_userdata.0', false, [stateBaseName + 'startTime', { 'name': 'Gultigkeitsbeginn (Uhrzeit)', 'type': 'string', 'read': true, 'write': false, 'role': 'state' }], function () {
+                    setState('0_userdata.0.' + stateBaseName + 'startTime', startTime, true);
+                });
+
+                createUserStates('0_userdata.0', false, [stateBaseName + 'endTime', { 'name': 'Gultigkeitsende (Uhrzeit)', 'type': 'string', 'read': true, 'write': false, 'role': 'state' }], function () {
+                    setState('0_userdata.0.' + stateBaseName + 'endTime', endTime, true);
+                });
+
+                createUserStates('0_userdata.0', false, [stateBaseName + 'power', { 'name': 'power', 'type': 'number', 'read': true, 'write': false, 'role': 'state', 'def': 0 }], function () {
+                    setState('0_userdata.0.' + stateBaseName + 'power', list[a].watt, true);
+                });
+
+                createUserStates('0_userdata.0', false, [stateBaseName + 'power90', { 'name': 'power90', 'type': 'number', 'read': true, 'write': false, 'role': 'state', 'def': 0 }], function () {
+                    setState('0_userdata.0.' + stateBaseName + 'power90', list[a].watt90, true);
+                });
+                }
+            }           
+        })
+        .catch((error) => {
+            console.error(error);
     });
-
 }
-
-// 10 request sind frei. hier 8 plus die 2 aus adapter
-
-schedule('0 8,10,11 * * *', function () {
-    const qurl = "/rooftop_sites/" + strasse +"/forecasts?format=json&api_key="+ key_id;
-    toLog('Hole PV Strassenseite', true);
-    requestData(qurl);
+// 10 request sind frei. hier 6 plus die 2 aus adapter
+schedule('1 8,10 * * *', function () {
+    const url = `${strasse}/forecasts?format=json&api_key=${key_id}`;
+//    toLog('Hole PV Strassenseite', true);
+    requestData(url);
 });
 
-schedule('0 12,13,14 * * *', function () {
-    const qurl = "/rooftop_sites/" + garten +"/forecasts?format=json&api_key="+ key_id;
-    toLog('Hole PV Gartenseite', true);
-    requestData(qurl);
+schedule('1 11,12,13,14 * * *', function () {
+    const url = `${garten}/forecasts?format=json&api_key=${key_id}`;
+//    toLog('Hole PV Gartenseite', true);
+    requestData(url);
 });
-
-
 
 
